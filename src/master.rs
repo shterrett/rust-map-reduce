c4fe2ce71ec8c910ade34655376b96907ff94009
Stuart Terrett <shterrett@gmail.com>
2017-02-23 21:52:22 -0500

    Master send reduce jobs from files

--- src/master.rs
+++ src/master.rs
@@ -1,14 +1,14 @@
 use std::path::PathBuf;
 use std::io::BufReader;
-use std::fs::File ;
+use std::fs::{ File, read_dir };
+use std::str::FromStr;
+use std::collections::HashMap;
 use worker::{ Job, JobResult };
 use chan;
 use chan::{ Sender, Receiver };
 
 struct Master {
     input_files: Vec<PathBuf>,
-    n_map: i32,
-    n_reduce: i32,
     working_directory: PathBuf,
     map: Box<Fn(BufReader<File>) -> Vec<String> + Send>,
     reduce: Box<Fn(Vec<BufReader<File>>) -> String + Send>,
@@ -21,19 +21,15 @@ struct Master {
 impl Master {
     fn new(working_directory: PathBuf,
            input_files: Vec<PathBuf>,
-           n_reduce: i32,
            map: Box<Fn(BufReader<File>) -> Vec<String> + Send>,
            reduce: Box<Fn(Vec<BufReader<File>>) -> String + Send>
            ) -> Self
     {
         let (work_send, work_recv) = chan::async();
         let (result_send, result_recv) = chan::async();
-        let n_map = input_files.iter().len() as i32;
 
         Master {
             input_files: input_files,
-            n_map: n_map,
-            n_reduce: n_reduce,
             working_directory: working_directory,
             map: map,
             reduce: reduce,
@@ -47,6 +43,31 @@ impl Master {
     fn do_map(&self) {
         for (index, input) in self.input_files.iter().enumerate() {
             self.job_queue.send(Job::Map(((index + 1) as i32, input.clone())));
+        }
+    }
+
+    fn do_reduce(&self) {
+        if let Ok(entries) = read_dir(self.working_directory.clone()) {
+            let groups = entries.filter_map(|entry| entry.ok())
+                                .fold(HashMap::new(), |mut grouped, entry| {
+                                    let _ = entry.file_name()
+                                                 .into_string()
+                                                 .and_then(|filename| {
+                                                     filename.split(".")
+                                                             .nth(3)
+                                                             .and_then(|i| {
+                                                                  i32::from_str(i).ok()
+                                                             }).ok_or(entry.file_name())
+                                                 })
+                                                 .map(|key| {
+                                                     let mut files = grouped.entry(key).or_insert(vec![]);
+                                                     files.push(entry.path())
+                                                 });
+                                    grouped
+                                });
+            for (index, group) in groups {
+                self.job_queue.send(Job::Reduce(((index + 1), group)));
+            }
         }
     }
 }
@@ -79,7 +100,6 @@ mod test {
             .collect::<Vec<PathBuf>>();
         let master = Master::new(working_directory.clone(),
                                  input_files.clone(),
-                                 4,
                                  Box::new(map_fn),
                                  Box::new(reduce_fn)
                                 );
@@ -97,5 +117,63 @@ mod test {
                                        .map(|(i, f)| Job::Map(((i + 1) as i32, f.clone())))
                                        .collect::<Vec<Job>>();
         assert_eq!(map_jobs.join().unwrap(), expected_jobs);
+    }
+
+    #[test]
+    fn master_enqueues_reduce_jobs() {
+        let working_directory = PathBuf::from("./test-data/master_enqueues_reduce_jobs");
+        let input_files = vec!["input_1", "input_2", "input_3", "input_4"].into_iter()
+            .map(|filename| {
+                let mut path = working_directory.clone();
+                path.push(filename);
+                path
+            })
+            .collect::<Vec<PathBuf>>();
+        let master = Master::new(working_directory.clone(),
+                                 input_files,
+                                 Box::new(map_fn),
+                                 Box::new(reduce_fn)
+                                );
+
+        let job_recv = master.worker_job_queue.clone();
+        let reduce_jobs = thread::spawn(move || {
+            job_recv.iter()
+                    .collect::<Vec<Job>>()
+                    .sort_by(|fst, snd| {
+                        match (fst, snd) {
+                            (&Job::Reduce((i, _)), &Job::Reduce((j, _))) => {
+                                i.cmp(&j)
+                            }
+                            _ => {
+                                1.cmp(&1)
+                            }
+                        }
+                    })
+        });
+
+        master.do_reduce();
+        drop(master);
+
+        let expected_jobs = (1..(4 + 1)).map(|reduce_id| {
+            (1..(4 + 1)).map(|map_id| {
+                let mut path = working_directory.clone();
+                path.push(format!("map.{}.reduce.{}", map_id, reduce_id));
+                path
+            }).collect::<Vec<PathBuf>>()
+        }).enumerate()
+          .map(|(i, f)| Job::Reduce(((i + 1) as i32, f.clone())))
+          .collect::<Vec<Job>>()
+          .sort_by(|fst, snd| {
+              match (fst, snd) {
+                  (&Job::Reduce((i, _)), &Job::Reduce((j, _))) => {
+                      i.cmp(&j)
+                  }
+                  _ => {
+                      1.cmp(&1)
+                  }
+              }
+          });
+
+        assert_eq!(reduce_jobs.join().unwrap(), expected_jobs);
     }
 }
